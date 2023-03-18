f99cefab7668e57449917f74c4e1d2e36720288f
DipakDai <dipakkumal673@gmail.com>
2023-03-18 09:19:51 +0545

    context-api completed

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,41 +1,29 @@
-import React, { useState } from "react";
-import products from "./Products";
-const Items = () => {
-  const [items, setItems] = useState(products);
+const Items = ({ id, img, price, description, title, quantity }) => {
   // console.log(items)
   return (
     <>
-      {items.map(({id,img,price,description,title,quantity}) => {
-        return (
-          <>
-          <div className="items-info" key={id}>
-            <div className="product-img">
-              <img
-                src={img}
-                alt="user"
-              />
-            </div>
-            <div className="title">
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
-            <div className="add-minus-quantity">
-              <i className="fa-solid fa-minus minus"></i>
-              <input type="text" placeholder={quantity} />
-              <i className="fa-solid fa-plus add"></i>
-            </div>
-            <div className="price">
-              <h3>Rs.{price}</h3>
-            </div>
+      <div className="items-info" key={id}>
+        <div className="product-img">
+          <img src={img} alt="user" />
+        </div>
+        <div className="title">
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <div className="add-minus-quantity">
+          <i className="fa-solid fa-minus minus"></i>
+          <input type="text" placeholder={quantity} />
+          <i className="fa-solid fa-plus add"></i>
+        </div>
+        <div className="price">
+          <h3>Rs.{price}</h3>
+        </div>
 
-            <div className="remove-item">
-              <i className="fas fa-trash-alt remove"></i>
-            </div>
-          </div>
-          <hr />
-          </>
-        );
-      })}
+        <div className="remove-item">
+          <i className="fas fa-trash-alt remove"></i>
+        </div>
+      </div>
+      <hr />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Cart from "./components/Cart";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Cart>
       <App />
-  </React.StrictMode>
+  </Cart>
 );
